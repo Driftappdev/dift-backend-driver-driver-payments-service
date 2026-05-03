df61233f0319aa08f1717b39fd4109c7ac8d81d0
driftappdeveloper <driftappdeveloper@users.noreply.github.com>
2026-05-04 06:36:27 +0700

    chore: enterprise unification and version bump v0.1.2

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,30 @@
 package main
 
 import (
+	"dift_backend_driver/driver-payments-service/internal/servicecore"
 	"fmt"
+	"log/slog"
 	"net/http"
 
 	"dift_backend_driver/driver-payments-service/config"
 	"dift_backend_driver/driver-payments-service/internal/handler"
 	"dift_backend_driver/driver-payments-service/internal/route"
 	"dift_backend_driver/driver-payments-service/internal/service"
-	"github.com/driftappdev/libpackage/gologger"
 )
 
 func main() {
+	_ = servicecore.NewEngineUnifiedBundle(servicecore.LoadEngineUnifiedConfigFromEnv("driver-payments-service"))
 	cfg, err := config.Load("config/config.yaml")
 	if err != nil {
 		panic(err)
 	}
-	logger := gologger.Default()
+	logger := slog.Default()
 	svc := service.NewPaymentsService()
 	h := handler.NewPaymentsHandler(svc)
 	mux := http.NewServeMux()
 	route.Register(mux, h)
 	addr := fmt.Sprintf(":%d", cfg.Server.HTTPPort)
-	logger.Info("driver-payments-service started", gologger.F("port", cfg.Server.HTTPPort))
+	logger.Info("driver-payments-service started", "port", cfg.Server.HTTPPort)
 	if err := http.ListenAndServe(addr, mux); err != nil && err != http.ErrServerClosed {
 		panic(err)
 	}
